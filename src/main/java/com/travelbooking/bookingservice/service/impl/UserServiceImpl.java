662ae3eb8dadd3b011f7f49946281f55ee0b161c
Sachin Saraswat <sachinsaraswat161@gmail.com>
2026-04-29 02:03:07 +0530

    Configured Spring Security to allow all requests and enabled BCrypt

--- src/main/java/com/travelbooking/bookingservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/travelbooking/bookingservice/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.travelbooking.bookingservice.service.impl;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.travelbooking.bookingservice.dto.UserRegistrationRequest;
@@ -11,16 +12,18 @@ import com.travelbooking.bookingservice.service.UserService;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository){
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void registerUser(UserRegistrationRequest request) {
         // 1. Check if email already exists
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new RuntimeException("Email already regitered.");
+            throw new RuntimeException("Email already registered.");
         }
 
         // 2. Map DTO -> Entity
@@ -28,7 +31,7 @@ public class UserServiceImpl implements UserService {
                     .firstName(request.getFirstName())
                     .lastName(request.getLastName())
                     .email(request.getEmail())
-                    .password(request.getPassword()) // encrypt it later
+                    .password(passwordEncoder.encode(request.getPassword()))
                     .phoneNumber(request.getPhoneNumber())
                     .dateOfBirth(request.getDateOfBirth())
                     .gender(request.getGender())
